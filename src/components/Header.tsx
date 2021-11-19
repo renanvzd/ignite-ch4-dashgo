3e1498158d0166e54a53d2198549cc03d2455dca
Renan Veronez Drechsler <renanvz@outlook.com>
2021-11-19 10:38:28 -0300

    Grafico

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
 import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
 
-export default function Header() {
+export function Header() {
   return (
     <Flex
       as="header"
