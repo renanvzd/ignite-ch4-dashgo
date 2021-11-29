41c15a6e9bea400c7d2077762156dd977cb0f7fd
Renan Veronez Drechsler <renanvz@outlook.com>
2021-11-29 18:43:46 -0300

    Listagem de usuarios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -11,7 +11,20 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
 
-    return data;
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      };
+    });
+
+    return users;
   })
 
   const isWideVersion = useBreakpointValue({
@@ -64,46 +77,22 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td p={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Renan Veronez</Text>
-                        <Text fontSize="sm" color="gray.300">renanvz@outlook.com</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>19 Nov. 2021</Td>}
-                  </Tr>
-                </Tbody>
-                <Tbody>
-                  <Tr>
-                    <Td p={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Renan Veronez</Text>
-                        <Text fontSize="sm" color="gray.300">renanvz@outlook.com</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>19 Nov. 2021</Td>}
-                  </Tr>
-                </Tbody>
-                <Tbody>
-                  <Tr>
-                    <Td p={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Renan Veronez</Text>
-                        <Text fontSize="sm" color="gray.300">renanvz@outlook.com</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>19 Nov. 2021</Td>}
-                  </Tr>
+                  {data.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td p={["4", "4", "6"]}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                      </Tr>
+                    )
+                  })}
                 </Tbody>
               </Table>
 
